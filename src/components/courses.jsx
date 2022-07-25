9c182e7f8333c75d5fab9a618d10af620f6e1428
jmurphy5613 <jmurphy5613@gmail.com>
2022-07-25 05:04:38 -0400

    added cards and added content

---
+++ src/components/courses.jsx
@@ -0,0 +1,87 @@
+import { Flex, Heading, Text, Button } from "@chakra-ui/react";
+
+
+
+export default function Courses() {
+    return (
+        <Flex
+            marginLeft={"7rem"}
+            direction={"column"}
+        >
+            <Heading
+                color="primary"
+                fontSize="5xl"
+            >Courses</Heading>
+            <Flex
+                width={"80%"}
+                marginTop={"2.5rem"}
+                justifyContent={"space-between"}
+            >
+                <Flex
+                    height={"35vh"}
+                    width={"20vw"}
+                    backgroundColor="#003B73"
+                    borderRadius={"22px"}
+                    direction={"column"}
+                    padding={"2rem"}
+                >
+                    <Heading
+                        color="#FFF6AA"
+                        fontSize="2xl"
+                        fontWeight={"400"}
+                        marginBottom={"1.5rem"}
+                    >MongoDB.</Heading>
+                    <Text
+                        color={"rgba(255, 246, 170, 0.7)"}
+                        fontWeight={"400"}
+                        fontSize="sm"                    
+                    >Use databases in your own website. Discover how data can be stored in DB's like Mongo and learn how to control the data.</Text>
+                </Flex>
+                <Flex
+                    height={"35vh"}
+                    width={"20vw"}
+                    backgroundColor="#05445E"
+                    borderRadius={"22px"}
+                    direction={"column"}
+                    padding={"2rem"}
+
+                >
+                    <Heading
+                        color="#FFF6AA"
+                        fontSize="2xl"
+                        fontWeight={"400"}
+                        marginBottom={"1.5rem"}
+
+                    >Web Dev.</Heading>
+                    <Text
+                        color={"rgba(255, 246, 170, 0.7)"}
+                        fontWeight={"400"}
+                        fontSize="sm"                
+                    >Create simple websites using HTML, CSS and JS. Host, publish and share your personalized website that you created from scratch.</Text>
+                </Flex>
+                <Flex
+                    height={"35vh"}
+                    width={"20vw"}
+                    backgroundColor="#145DA0"
+                    borderRadius={"22px"}
+                    direction={"column"}
+                    padding={"2rem"}
+                >
+                    <Heading
+                        color="#FFF6AA"
+                        fontSize="2xl"
+                        fontWeight={"400"}
+                        marginBottom={"1.5rem"}
+
+                    >Game Dev.</Heading>
+                    <Text
+                        fontWeight={"400"}
+                        fontSize="sm"
+                        color={"rgba(255, 246, 170, 0.7)"}
+                    >Learn how to create simple games, tic tac toe, quiz games and more. You will learn how to create real-world applications using languages like Javascript.</Text>
+                    <Button></Button>
+                </Flex>
+            </Flex>
+        </Flex>
+    )
+}
