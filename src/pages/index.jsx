330b5713517fd281493652db7dd590d8a43ad1b2
PranTanTheMan <46548127+PranTanTheMan@users.noreply.github.com>
2022-07-25 20:27:23 -0500

    margin top and bottom

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,13 +2,16 @@ import Footer from "../components/footer";
 import Header from "../components/header";
 import HeroHeader from "../components/homepage-landing";
 import Courses from "../components/courses";
+import { Box } from "@chakra-ui/react";
 
 export default function Home() {
   return (
     <>
-      <Header />
-      <HeroHeader />
-      <Courses />
+      <Box my="50px">
+        <Header />
+        <HeroHeader />
+        <Courses />
+      </Box>
       <Footer />
     </>
   );
