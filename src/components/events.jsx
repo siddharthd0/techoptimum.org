d0547e683dbaa063ada32a3025281895f5d1205f
Siddharth Duggal <siddharthduggal2013@gmail.com>
2022-07-30 11:42:48 -0600

    idk

--- src/components/events.jsx
+++ src/components/events.jsx
@@ -12,13 +12,13 @@ export default function Events() {
       <Head>
         <link href="https://unpkg.com/aos@next/dist/aos.css" rel="stylesheet" key="test"/>
       </Head>
-      <Heading color="primary" fontSize="5xl" data-aos="fade-right" data-aos-duration="1000">
+      <Heading color="primary" fontSize="5xl" data-aos="fade-left" data-aos-duration="1000">
         Events
       </Heading>
       <Text color="primary" fontSize="3xl" marginTop={"2.5rem"} data-aos="fade-right" data-aos-duration="1000">
         Participate in events hosted by Tech Optimum
       </Text>
-      <Text color="primary" fontSize="xl" marginTop={"2.5rem"} width={"70%"} data-aos="fade-right" data-aos-duration="1000">
+      <Text color="primary" fontSize="xl" marginTop={"2.5rem"} width={"70%"} data-aos="fade-left" data-aos-duration="1000">
         We host yearly hackathons. A hackathon is 48-hour coding competition in
         which participants can code projects as a team or individually and
         competet for numerous prizes!. Not only that but, we host fun
