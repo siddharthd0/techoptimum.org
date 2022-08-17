56544f1b0dea27509388e9336305b32078b08abc
Taptaplit <pateltaptaplit@gmail.com>
2022-08-16 21:03:20 -0400

    created join page landing component

--- src/pages/join.jsx
+++ src/pages/join.jsx
@@ -1,22 +1,16 @@
 import Footer from "../components/footer";
 import Header from "../components/header";
 import Socials from "../components/socials";
-import HackathonRecap from "../components/hackathon-recap.jsx";
-import HackathonBanner from "../assets/hackathon2022.svg";
 import { Box } from "@chakra-ui/react";
-import HackathonWinners from "../components/hackathon-winners";
-import UpcomingEvents from "../components/upcoming-events";
+import JoinHero from "../components/joinpage-landing";
 
 
 export default function Hackathons() {
 	return (
 		<>
 			<Header />
-  			<HackathonBanner width="100%" />
-			<Box my="100px">
-				<HackathonRecap />
-				<HackathonWinners/>
-				<UpcomingEvents/>
+			<Box mb="100px">
+				<JoinHero />
 			</Box>
 			<Socials />
 			<Footer />
