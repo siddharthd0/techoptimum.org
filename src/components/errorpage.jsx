e7af175d11f77ffba535b0601cced9b08fb0bc2a
Siddharth <96356209+siddharthd0@users.noreply.github.com>
2022-08-26 00:48:42 -0600

    nice links

--- src/components/errorpage.jsx
+++ src/components/errorpage.jsx
@@ -9,58 +9,67 @@ export default function Error() {
         <Flex display={"flex"} direction={"column"}>
           <Flex direction={"column"} width={"70%"} margin={"1rem 0 0 11.9rem"}>
             <Heading color="white" fontSize="40px" marginBottom={"2.5rem"}>
-                Wr④⓪④ng way, friend.
+                Wr④⓪④ng way, Friend.
             </Heading>
             <Text
               fontSize="2xl"
               color="primary"
-              marginBottom={"2.5rem"}
+              marginBottom={"1rem"}
               width={"85%"}
             >
                 These might help you:
             </Text>
             <Button
-              backgroundColor="transparent"
-              _hover={{ backgroundColor: "transparent" }}
-              width="fit-content"
-              padding={"0"}
-              color="#A7B2FF"
-              fontWeight={"400"}
-              fontSize={"24px"}
-              rightIcon={
-                <ExternalLinkIcon position={"relative"} bottom={"2px"} />
-              }
-            >
-            <Link href="/">Home</Link>
-            </Button>
-            <Button
-              backgroundColor="transparent"
-              _hover={{ backgroundColor: "transparent" }}
-              width="fit-content"
-              padding={"0"}
-              color="#A7B2FF"
-              fontWeight={"400"}
-              fontSize={"24px"}
-              rightIcon={
-                <ExternalLinkIcon position={"relative"} bottom={"2px"} />
-              }
-            >
-              Contact
-            </Button>
-            <Button
-              backgroundColor="transparent"
-              _hover={{ backgroundColor: "transparent" }}
-              width="fit-content"
-              padding={"0"}
-              color="#A7B2FF"
-              fontWeight={"400"}
-              fontSize={"24px"}
-              rightIcon={
-                <ExternalLinkIcon position={"relative"} bottom={"2px"} />
-              }
-            >
-              Dashboard
-            </Button>
+            backgroundColor="transparent"
+            _hover={{ 
+              backgroundColor: "transparent" ,
+              color: "#9DB2F6",
+            }}
+            width="fit-content"
+            padding={"0"}
+            color="#1B76FF"
+            fontWeight={"400"}
+         
+            rightIcon={
+              <ExternalLinkIcon position={"relative"} bottom={"2px"} />
+            }
+          >
+            HOME
+          </Button>
+          <Button
+            backgroundColor="transparent"
+            _hover={{ 
+              backgroundColor: "transparent" ,
+              color: "#9DB2F6",
+            }}
+            width="fit-content"
+            padding={"0"}
+            color="#1B76FF"
+            fontWeight={"400"}
+         
+            rightIcon={
+              <ExternalLinkIcon position={"relative"} bottom={"2px"} />
+            }
+          >
+           CONTACT
+          </Button>
+          <Button
+            backgroundColor="transparent"
+            _hover={{ 
+              backgroundColor: "transparent" ,
+              color: "#9DB2F6",
+            }}
+            width="fit-content"
+            padding={"0"}
+            color="#1B76FF"
+            fontWeight={"400"}
+         
+            rightIcon={
+              <ExternalLinkIcon position={"relative"} bottom={"2px"} />
+            }
+          >
+            DASHBOARD
+          </Button>
           </Flex>
         </Flex>
         <Image width={"520px"} height={"550px"} src={"/robot_404.png"}  />
