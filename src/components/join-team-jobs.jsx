300b12b80cf876b797319be145131fa6abdd8b66
Siddharth <96356209+siddharthd0@users.noreply.github.com>
2022-08-31 23:11:10 -0600

    started jobs section

---
+++ src/components/join-team-jobs.jsx
@@ -0,0 +1,33 @@
+import { Image, Box, Button, Flex, Heading, Text } from "@chakra-ui/react";
+import { BsArrowUpRight } from "react-icons/bs";
+import { ExternalLinkIcon } from "@chakra-ui/icons";
+export default function JoinTeamJobs() {
+  return (
+    <>
+    
+      <Flex flexDir="column"width="50%"height={"600px"} backgroundImage={"https://cdn.discordapp.com/attachments/910672979401269258/1014761300431208578/Rectangle_5939.png"} >
+     <Heading pt="4rem" textAlign={"center"} marginX="auto" className="development-heading">Development</Heading>
+     <Text mt={"2rem"} mb="2rem"width={"60%"}  marginX="auto"textAlign={"center"} color={"#495DE6"}>As a website developer, you will be working
+on the Tech optimum Website to  
+improve & maintain it.</Text>
+     <Button
+            backgroundColor="transparent"
+            _hover={{ 
+              backgroundColor: "transparent" ,
+              color: "#9DB2F6",
+            }}
+          
+            padding={"0"}
+            color="#1B76FF"
+            fontWeight={"400"}
+            marginX={"auto"}
+            rightIcon={
+              <ExternalLinkIcon position={"relative"} bottom={"2px"} />
+            }
+          >
+            LEARN MORE
+          </Button>
+      </Flex>
+    </>
+  );
+}
--- src/pages/join-team.jsx
+++ src/pages/join-team.jsx
@@ -3,13 +3,15 @@ import Header from "../components/header";
 import Socials from "../components/socials";
 import { Box } from "@chakra-ui/react";
 import JoinTeamLanding from '../components/join-team-landing';
+import JoinTeamJobs from "../components/join-team-jobs";
 
 export default function JoinTeam() {
   return (
     <>
       <Header />
-      <Box zIndex={"2"} position="relative" top="-120px" mb="100px" overflow="hidden">
+      <Box zIndex={"2"} position="relative" top="-120px" overflow="hidden">
         <JoinTeamLanding />
+        <JoinTeamJobs />
       </Box>
       <Socials />
       <Footer />
