59a20d4a7af57fe291d3fcda306bb84c7507e57e
Siddharth <96356209+siddharthd0@users.noreply.github.com>
2022-11-19 17:54:44 -0700

    curriculm landing finished

---
+++ src/components/curriculm-landing.jsx
@@ -0,0 +1,65 @@
+import { ExternalLinkIcon } from "@chakra-ui/icons";
+import CountUp from "react-countup";
+import { motion } from "framer-motion";
+import { Link } from "@chakra-ui/react";
+import { BsArrowUpRight, BsBox } from "react-icons/bs";
+import {
+  Box,
+  Container,
+  Flex,
+  Heading,
+  Text,
+  Button,
+  Icon,
+} from "@chakra-ui/react";
+
+export default function CurriculmLanding() {
+  return (
+    <>
+      <Box className="curriculmbg">
+        <Flex direction="row">
+          <Flex
+            marginLeft={["2rem", "7rem"]}
+            marginTop={"5rem"}
+            direction={"column"}
+            width="50%;"
+          >
+            <Heading fontSize={"5xl"} color="primary" marginBottom={"1rem"}>
+              {" "}
+              Tech Optimum Curriculm
+            </Heading>
+            <Text marginBottom="2rem">
+              Taking an effective and efficient approach to computer science is
+              integral to our philosophy. To practice what you have learned, you
+              should be able to do practical programming projects. All you need
+              is a willingness to learn; we'll help you do the rest!
+            </Text>
+            <Button width="130px" fontWeight={"light"} colorScheme="buttonBg">
+              Learn More
+            </Button>
+          </Flex>
+          <Flex
+            borderRadius={"15px"}
+            height="300px"
+            width="400px"
+            backgroundColor={"#2E3569"}
+            marginTop={"4rem"}
+            marginLeft={["4rem", "5rem"]}
+            direction={"column"}
+            padding={"1.5rem"}
+          >
+            <Heading marginTop={"1rem"} fontSize={"3xl"} color="#FE9292">Intro to Web Dev.</Heading>
+            <Text>4 weeks · Introductory Course</Text>
+            <br/>
+            <Heading fontSize={"3xl"} color="#FE9292">Intro to Game Dev.</Heading>
+            <Text>6 weeks · Intermediate Course</Text>
+            <br/>
+            <Heading fontSize={"3xl"} color="#FE9292">Intro to Python</Heading>
+            <Text>2 weeks · Introductory Course</Text>
+            <br/>
+          </Flex>
+        </Flex>
+      </Box>
+    </>
+  );
+}
---
+++ src/pages/curriculm.jsx
@@ -0,0 +1,16 @@
+import CurriculmLanding from "../components/curriculm-landing";
+import Footer from "../components/footer";
+import Header from "../components/header";
+import Socials from "../components/socials";
+
+
+export default function Curriculm() {
+  return (
+    <>
+      <Header />
+      <CurriculmLanding/>
+ 
+      <Footer />
+    </>
+  );
+}
