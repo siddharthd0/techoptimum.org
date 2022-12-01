3aaa731f1830ea01d99577e5d1303a56f095ff9b
Siddharth <96356209+siddharthd0@users.noreply.github.com>
2022-12-01 14:35:30 -0700

    stuff

--- src/components/joinpage-landing.jsx
+++ src/components/joinpage-landing.jsx
@@ -11,7 +11,7 @@ export default function JoinHero() {
         flexDir="column"
         justifyContent="space-around"
         alignItems="center"
-        height={["80vh","100vh"]}
+        height={["70vh","80vh"]}
         width="100vw"
         paddingTop="5%"
         paddingBottom="10%"
@@ -20,28 +20,20 @@ export default function JoinHero() {
       >
         <Heading
         
-          marginBottom={["-50px", "-10px"]}
-          marginTop={"140px "}
+          marginBottom={["-50px", "-20px"]}
+          marginTop={"30px "}
           fontWeight={100}
           fontSize={["4xl", "6xl"]}
 
         >
           Tech Optimum
-        </Heading>
-        <Heading
-          fontWeight={100}
-          color={"blue.100"}
-          marginTop={"-10px"}
-          px={"10px"}
-          borderRadius={"10px"}
-          backgroundColor={"blue.500"}
-          fontSize={["4xl", "6xl"]}
-        >
+          <br/>
           Community
         </Heading>
-        <Text  fontSize={["lg", "2xl"]}>
-          Join a community of like-minded <br />
-          people who enjoy coding.
+      
+        <Text px="20px" fontSize={["lg", "2xl"]}>
+          Join our Discord community of like-minded <br />
+          people who enjoy coding and building new projects!
         </Text>
         <Link
           _hover={{
@@ -51,10 +43,10 @@ export default function JoinHero() {
           isExternal
         >
           <Button
-          marginTop={["-70px", "-10px"]}
+         marginTop={"-30px "}
             colorScheme="buttonBg"
-            padding={["20px 25px 20px 25px","25px 35px 25px 35px"]}
-            fontSize={["lg", "2xl"]}
+            padding={["20px 25px 20px 25px","20px 30px 20px 30px"]}
+            fontSize={["lg", "xl"]}
             fontWeight="normal"
             rounded="full"
             rightIcon={
