45dc207c22f199baaf9708cef71e4549edf651d1
Siddharth <96356209+siddharthd0@users.noreply.github.com>
2022-12-01 23:41:02 -0700

    Fixed build error on navbar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -12,7 +12,7 @@ import {
   Spacer,
   NextChakraLink,
   SimpleButton,
-  Image
+  Image,
 } from "@chakra-ui/react";
 
 import Link from "next/link";
@@ -42,7 +42,7 @@ export default function Header() {
         />
         <title>Tech Optimum</title>
       </Head>
-    {/*  <Flex
+      {/*  <Flex
         bgColor="#FFAC33"
         color="black"
         py={1.5}
@@ -65,108 +65,128 @@ export default function Header() {
       <Flex
         zIndex="100 !important"
         justifyContent="space-between"
-      
         padding="15px 12%"
         position={"relative"}
         direction={["column", "row"]}
         alignItems="center"
-        
       >
         <Flex alignItems={"center"} justifyContent={"start"}>
-        <a href="/">
-        <Image display={["none", "block"]}  w="60px" src="./logo-transparent.png"></Image>
-        </a>
-        <a href="/">
-        <Heading marginTop={"7px"} textAlign={"center"}  alignItems="left" href="https://techoptimum.org" fontSize="3xl" color="primary">
-          Tech Optimum
-        </Heading>
-        </a>
+          <Link _hover={{}} href={"./"}>
+            <Image
+              _hover={{
+                cursor: "pointer",
+              }}
+              display={["none", "block"]}
+              w="60px"
+              src="./logo-transparent.png"
+            ></Image>
+          </Link>
+          <Link href={"./"}>
+            <Heading
+              _hover={{
+                cursor: "pointer",
+              }}
+              marginTop={"7px"}
+              textAlign={"center"}
+              alignItems="left"
+              href="https://techoptimum.org"
+              fontSize="3xl"
+              color="primary"
+            >
+              Tech Optimum
+            </Heading>
+          </Link>
         </Flex>
-        <Flex   marginTop={["1.5rem", "2.1px"]} textAlign={"center"}>
-        <nav>
-          <ul className="nav-links">
-            <li>
-              <Text className="link-navs" color="primary">
-                <Link href="/">HOME</Link>
-              </Text>
-            </li>
-            <li>
-              <Menu gutter={"5"} isOpen={isEditOpen}>
-                <MenuButton
-                  className="link-navs"
-                  bgColor={"transparent"}
-                  color="primary"
-                  onMouseEnter={onEditOpen}
-                  onMouseLeave={onEditClose}
-                >
-                  ABOUT
-                </MenuButton>
-                <div className="testing-nav">
-                  <MenuList
+
+        <Flex marginTop={["1.5rem", "2.1px"]} textAlign={"center"}>
+          <nav>
+            <ul className="nav-links">
+              <li>
+                <Text className="link-navs" color="primary">
+                  <Link href="/">HOME</Link>
+                </Text>
+              </li>
+              <li>
+                <Menu gutter={"5"} isOpen={isEditOpen}>
+                  <MenuButton
+                    className="link-navs"
+                    bgColor={"transparent"}
+                    color="primary"
                     onMouseEnter={onEditOpen}
                     onMouseLeave={onEditClose}
+                  >
+                    ABOUT
+                  </MenuButton>
+                  <div className="testing-nav">
+                    <MenuList
+                      onMouseEnter={onEditOpen}
+                      onMouseLeave={onEditClose}
+                      className="link-navs-dropdown"
+                    >
+                      <Link href="/about">
+                        <MenuItem className="link-navs-dd-text">
+                          Meet the Team
+                        </MenuItem>
+                      </Link>
+                      <Link href="/contact">
+                        <MenuItem className="link-navs-dd-text">
+                          Contact us
+                        </MenuItem>
+                      </Link>
+                      <Link href="/faq">
+                        <MenuItem className="link-navs-dd-text">Faq</MenuItem>
+                      </Link>
+                    </MenuList>
+                  </div>
+                </Menu>
+              </li>
+              <li>
+                <Menu gutter={"5"} isOpen={isSecondOpen}>
+                  <MenuButton
+                    className="link-navs"
+                    bgColor={"transparent"}
+                    color="primary"
+                    onMouseEnter={onSecondOpen}
+                    onMouseLeave={onSecondClose}
+                  >
+                    INITIATIVES
+                  </MenuButton>
+                  <MenuList
+                    onMouseEnter={onSecondOpen}
+                    onMouseLeave={onSecondClose}
                     className="link-navs-dropdown"
                   >
-                    <Link href="/about">
+                    <Link href="./curriculum">
                       <MenuItem className="link-navs-dd-text">
-                        Meet the Team
+                        Curriculum
                       </MenuItem>
                     </Link>
-                    <Link href="/contact">
+                    <Link href="/join">
                       <MenuItem className="link-navs-dd-text">
-                        Contact us
+                        Community
                       </MenuItem>
                     </Link>
-                    <Link href="/faq">
-                      <MenuItem className="link-navs-dd-text">Faq</MenuItem>
+                    <Link href="/hackathon">
+                      <MenuItem className="link-navs-dd-text">
+                        Tech Optimum Hacks
+                      </MenuItem>
+                    </Link>
+                    <Link href="/podcast">
+                      <MenuItem className="link-navs-dd-text">
+                        Tech Optimum Talks
+                      </MenuItem>
                     </Link>
                   </MenuList>
-                </div>
-              </Menu>
-            </li>
-            <li>
-              <Menu gutter={"5"} isOpen={isSecondOpen}>
-                <MenuButton
-                  className="link-navs"
-                  bgColor={"transparent"}
-                  color="primary"
-                  onMouseEnter={onSecondOpen}
-                  onMouseLeave={onSecondClose}
-                >
-                  INITIATIVES
-                </MenuButton>
-                <MenuList
-                  onMouseEnter={onSecondOpen}
-                  onMouseLeave={onSecondClose}
-                  className="link-navs-dropdown"
-                >
-                  <Link href="./curriculum">
-                    <MenuItem className="link-navs-dd-text">Curriculum</MenuItem>
-                  </Link>
-                  <Link href="/join">
-                    <MenuItem className="link-navs-dd-text">Community</MenuItem>
-                  </Link>
-                  <Link href="/hackathon">
-                    <MenuItem className="link-navs-dd-text">Tech Optimum Hacks</MenuItem>
-                  </Link>
-                  <Link href="/podcast">
-                    <MenuItem className="link-navs-dd-text">
-                      Tech Optimum Talks
-                    </MenuItem>
-                  </Link>
-                  
-                </MenuList>
-              </Menu>
-            </li>
-            <li>
-              <Text className="link-navs" color="primary">
-                <Link href="/join-team">VOLUNTEER</Link>
-              </Text>
-            </li>
-          </ul>
-        </nav>
+                </Menu>
+              </li>
+              <li>
+                <Text className="link-navs" color="primary">
+                  <Link href="/join-team">VOLUNTEER</Link>
+                </Text>
+              </li>
+            </ul>
+          </nav>
         </Flex>
-        
       </Flex>
     </>
   );
