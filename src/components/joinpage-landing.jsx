f73a67c26fa1fb12afb259d7cace1ddc65b92d66
Siddharth <96356209+siddharthd0@users.noreply.github.com>
2022-12-08 17:34:15 -0700

    made the join page look hotter

--- src/components/joinpage-landing.jsx
+++ src/components/joinpage-landing.jsx
@@ -11,27 +11,47 @@ export default function JoinHero() {
         flexDir="column"
         justifyContent="space-around"
         alignItems="center"
-        height={["70vh","80vh"]}
+        height={["70vh", "80vh"]}
         width="100vw"
         paddingTop="5%"
         paddingBottom="10%"
         marginTop="0"
-       
       >
-        <Heading
-        
-          marginBottom={["-50px", "-20px"]}
-          marginTop={"30px "}
-          fontWeight={100}
-          fontSize={["4xl", "6xl"]}
-
+       
+        <svg
+          id="stroke"
+          xmlns="http://www.w3.org/2000/svg"
+          width="0"
+          height="0"
         >
-          Tech Optimum
-          <br/>
+          <defs>
+            <path
+              id="line"
+              d="M2 2c49.7 2.6 100 3.1 150 1.7-46.5 2-93 4.4-139.2 7.3 45.2-1.5 90.6-1.8 135.8-.6"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              vector-effect="non-scaling-stroke"
+            />
+          </defs>
+        </svg>
+
+       <Heading  marginBottom={["-50px", "-40px"]}
+          marginTop={"30px "}
+          fontWeight={1000}
+          fontSize={["4xl", "6xl"]}>Tech Optimum</Heading>
+
+        <Text fontWeight="1000" fontSize={["4xl", "49px"]}className="wavy" href="/">
           Community
-        </Heading>
-      
-        <Text px="20px" fontSize={["lg", "2xl"]}>
+          <svg class="button-stroke" viewBox="0 0 154 13">
+            <use href="#line"></use>
+          </svg>
+          <svg class="button-stroke" viewBox="0 0 154 13">
+            <use href="#line"></use>
+          </svg>
+        </Text>
+
+        <Text px="20px" fontSize={["lg", "xl"]}>
           Join our Discord community of like-minded <br />
           people who enjoy coding and building new projects!
         </Text>
@@ -43,11 +63,11 @@ export default function JoinHero() {
           isExternal
         >
           <Button
-         marginTop={"-30px "}
+         
             colorScheme="buttonBg"
-            padding={["20px 25px 20px 25px","20px 30px 20px 30px"]}
+            padding={["20px 25px 20px 25px", "20px 30px 20px 30px"]}
             fontSize={["lg", "xl"]}
-            fontWeight="normal"
+           fontWeight="normal"
             rounded="full"
             rightIcon={
               <ExternalLinkIcon position={"relative"} bottom={"2px"} />
