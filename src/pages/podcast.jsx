1cf0fc7a765004885c70bf3ee2ef1c1f1b062625
Siddharth <96356209+siddharthd0@users.noreply.github.com>
2022-12-10 13:46:15 -0700

    Alert for podcast page

--- src/pages/podcast.jsx
+++ src/pages/podcast.jsx
@@ -1,15 +1,33 @@
 import Footer from "../components/footer";
 import Header from "../components/header";
 import Socials from "../components/socials";
-import { Box } from "@chakra-ui/react";
+import { Text,Box , ModalHeader, useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalCloseButton,
+  ModalBody,} from "@chakra-ui/react";
 import PodcastHero from "../components/podcast-landing";
 import PodcastPlayer from "../components/podcast-player";
 
 export default function Podcasts() {
+  const { isOpen, onClose } = useDisclosure({ defaultIsOpen: true })
   return (
     <>
       <Header />
       <Box mb="100px">
+          <Modal isOpen={isOpen} onClose={onClose} isCentered>
+                <ModalOverlay />
+                <ModalContent backgroundColor={"blue.800"}>
+      
+                    <ModalCloseButton />
+                    <ModalHeader color={"white"}>Under Construction</ModalHeader>
+                    <ModalBody>
+                        <Text>Our team is currently working hard to finish rework this page. Some buttons may not work, and information is not up-to-date. Please come back later!</Text>
+                    </ModalBody>
+                    <br/>
+                </ModalContent>
+            </Modal>
         <PodcastHero />
         <PodcastPlayer />
       </Box>
