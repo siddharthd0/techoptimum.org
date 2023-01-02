b0375e1489a3c6c23f05371f748b34b05cb36213
Siddharth <96356209+siddharthd0@users.noreply.github.com>
2023-01-02 16:41:57 -0700

    Fixed spacing and typos in testimonials

--- src/components/testimonials-card.jsx
+++ src/components/testimonials-card.jsx
@@ -9,30 +9,32 @@ export default function TestimonialCard() {
   }, []);
   const testimonials = [
     {
-      name: "Kaushal Kurpad",
+      name: "Kaushal Kurpad, Student",
       title: "Community Member",
       description:
         "Tech Optimum has the best community for coding. The staff are wonderful and experienced! They will help you with any problem you have. I would recommend Tech Optimum to anyone who wants to learn coding.",
       src: "https://ui-avatars.com/api/?background=72a9ed&color=fff&name=Kaushal+Kurpad&size=100",
     },
     {
-      name: "Aditya Sahasranam",
+      name: "Aditya Sahasranam, Student",
       title: "Community Manager",
       description:
         "Tech Optimum is an incredible organization that is making a real difference in the world. Its commitment to its community, its dedication to providing access to technology and education, and its exceptional team make it truly special and a worthy cause.",
       src: "https://cdn.discordapp.com/attachments/916543942139469884/1057191127016542238/Screen_Shot_2022-12-27_at_12.00.00_AM.png",
     },
     {
-      name: "Viachaslau Matsukevich",
+      name: "Viachaslau M., Software Engineer",
       title: "Hackathon Judge",
       description:
-        "I had a great time Judging in the Tech Optimum Hacks. Before that, I participated in 15+ hackathons and can say that this was an awesome and well-organized event. Thanks to the Hackathon, lot's of great project with good potential was created. I think this event would be great for experienced hackers as well as novices. I'd like to thank Tech Optimum team for the opportunity and would love to participate again",
-      src: "https://ui-avatars.com/api/?background=72a9ed&color=fff&name=Viachaslau+Matsukevich&size=100",
+        "I had a great time judging for Tech Optimum Hacks. Before that, I participated in 15+ hackathons and can say that this was an awesome and well-organized event. Thanks to the Hackathon, lot's of great project with good potential was created. This event was great for experienced hackers as well as novices. I'd like to thank Tech Optimum team for the opportunity and would love to participate again! ",
+      src: "https://media.discordapp.net/attachments/996113402902614126/996114010351091763/headshot-matsukevich2.jpg?width=858&height=1045",
     },
   ];
   return (
     <>
       <Flex
+        margin="auto"
+        maxWidth="1000px"
         overflow={"none"}
         justifyContent={"center"}
         flexWrap={"wrap"}
@@ -41,6 +43,7 @@ export default function TestimonialCard() {
       >
         {testimonials.map((testimonial, i) => (
           <Box
+            mt="20px"
             key={i}
             w="md"
             py={4}
