d0400568395e2b1defb4bb5f88f90c05961ba413
Siddharth <96356209+siddharthd0@users.noreply.github.com>
2023-01-17 21:43:38 -0700

    New landing join page finished

--- src/pages/join.jsx
+++ src/pages/join.jsx
@@ -1,17 +1,81 @@
 import Footer from "../components/footer";
 import Header from "../components/header";
 import Socials from "../components/socials";
-import { Box } from "@chakra-ui/react";
-import JoinHero from "../components/discord-page-landing";
-import JoinDiscord from "../components/join-discord";
+import { Text, Heading,Tooltip, Flex, Link, Button, Box } from "@chakra-ui/react";
 
 export default function Join() {
   return (
     <>
       <Header />
-      <Box mb="100px">
-        <JoinHero />
-        <JoinDiscord />
+      <Box
+        height={"80vh"}
+        backgroundImage={"/join-landing.png"}
+        backgroundSize={"cover"}
+        backgroundPosition={"center"}
+        backdropFilter={"blur(10px)"}
+      >
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <Box
+          marginX="auto"
+          className="greenbg"
+          borderRadius={"20px"}
+          maxWidth="800px"
+          padding="2rem"
+          opacity={".95"}
+        >
+          <Heading fontSize={"5xl"} color={"blue.900"}>
+            We are Tech Optimum
+          </Heading>
+          <Text
+            mt=".5rem"
+            mb="1rem"
+            maxW={"70%"}
+            fontSize={"lg"}
+            color={"blue.900"}
+          >
+            A platform where you can innovate, learn, create, and share your
+            projects with our community.
+          </Text>
+          <Link
+            _hover={{
+              textDecoration: "none",
+            }}
+            href="#do"
+          >
+            <Button
+              mr={"18px"}
+              px={"25px"}
+              borderRadius={"20px"}
+              colorScheme={"teal"}
+              fontWeight="light"
+            >
+              {" "}
+              Learn more about what we do{" "}
+            </Button>
+          </Link>
+          <Link
+            _hover={{
+              textDecoration: "none",
+            }}
+            href="/discord"
+            isExternal
+          >
+            <Button
+              px={"25px"}
+              borderRadius={"20px"}
+              colorScheme={"teal"}
+              fontWeight="light"
+            >
+              {" "}
+              Join our Discord{" "}
+            </Button>
+          </Link>
+        </Box>
       </Box>
       <Socials />
       <Footer />
