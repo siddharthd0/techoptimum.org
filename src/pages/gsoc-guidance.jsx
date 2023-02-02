36e7ae807ef99ec3faf516d7657a643f1c74420c
Siddharth <96356209+siddharthd0@users.noreply.github.com>
2023-02-02 00:08:28 -0700

    gsoc guidelines ++

---
+++ src/pages/gsoc-guidance.jsx
@@ -0,0 +1,3 @@
+import redirect from "nextjs-redirect";
+export default redirect("https://techoptimum.notion.site/GSoC-Guidelines-50d258acddf94931973c77ccad2ef4e9");
+// Why you don't work? I don't know. But it works.
