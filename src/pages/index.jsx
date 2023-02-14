590fb95567220fe9433eb856cd2c0a9352c6d776
Siddharth <96356209+siddharthd0@users.noreply.github.com>
2023-02-13 22:12:45 -0700

    Notification reminder to check announcements added

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,16 +6,57 @@ import Jobs from "../components/jobs";
 import Events from "../components/events";
 import Finder from "../components/internship-finder";
 import Socials from "../components/socials";
-import { Box } from "@chakra-ui/react";
 import Mission from "../components/mission";
 import TestimonialCard from "../components/testimonials-card";
+import { useEffect } from "react";
+import { useToast, Button, Box } from "@chakra-ui/react";
+import Link from "next/link";
 //redepoylkafmkfm
 export default function Home() {
+  const toast = useToast();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      toast.closeAll();
+    }, 50000);
+    toast({
+      position: "bottom-right",
+
+      render: () => (
+       <Link href="announcements">
+        <Box
+        transition={"300ms"}
+        _hover={{
+          cursor: "pointer",
+          transform: 'scale(1.1)',
+        }} 
+          borderRadius={"10px"}
+          mb="1rem"
+          mx="1.5rem"
+          color="white"
+          p={3}
+          bg="#29368C"
+          px="1.5rem"
+        >
+          Check out our latest announcements →
+         
+        </Box>
+         </Link>
+      ),
+    });
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
   return (
     <>
+      {/* <Button onClick={() => toast.closeAll()}>Close Toast</Button>*/}
       <Header />
       <meta name="og:title" content="Tech Optimum "></meta>
-      <meta name="og:description" content="A student-led organization dedicated to helping and inspiring the next generation of tech leaders."></meta>
+      <meta
+        name="og:description"
+        content="A student-led organization dedicated to helping and inspiring the next generation of tech leaders."
+      ></meta>
       <meta name="og:image" content=" /meta.png"></meta>
       <Box my={["20px", "65px"]} _hover={{}}>
         <HeroHeader />
