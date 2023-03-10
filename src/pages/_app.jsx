4008911ad054e60b12796290e56805d68e3b5fc8
nurselaine <Elaine.t.huynh@gmail.com>
2023-03-10 11:39:35 -0800

    Added page loader to display while DOM loads

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,11 +3,29 @@ import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../styles/theme";
 import Fonts from "../components/font";
 import { Analytics } from "@vercel/analytics/react";
+
+import Loading from "./loading";
+import { useRouter } from "next/router";
+import { useState } from "react";
+
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
+  router.events.on('routeChangeStart', (url) => {
+    setLoading(true);
+   });
+
+  router.events.on('routeChangeComplete', (url) => {
+    setLoading(false);
+  });
+
   return (
     <ChakraProvider theme={theme}>
       <Fonts />
-      <Component {...pageProps} />
+      {
+        loading ? <Loading /> : <Component {...pageProps} /> 
+      }
       <Analytics />
     </ChakraProvider>
   );
