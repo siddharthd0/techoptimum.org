e0de7927c106f2ae8497aba181324589cd1375b7
nurselaine <Elaine.t.huynh@gmail.com>
2023-03-10 11:54:29 -0800

    Added useEffect hook to update loading state on router event changes

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,14 +1,13 @@
 import "../styles/globals.css";
-import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../styles/theme";
 import Fonts from "../components/font";
+
 import { Analytics } from "@vercel/analytics/react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 import Loading from "./loading";
-import { useRouter } from "next/router";
-import { useState } from "react";
-
-
 import Header from "../components/header";
 import Footer from "../components/footer";
 
@@ -16,20 +15,21 @@ function MyApp({ Component, pageProps }) {
   const router = useRouter();
   const [loading, setLoading] = useState(false);
 
-  router.events.on('routeChangeStart', (url) => {
-    setLoading(true);
-   });
-
-  router.events.on('routeChangeComplete', (url) => {
-    setLoading(false);
-  });
+  useEffect(() => {
+    router.events.on('routeChangeStart', (url) => {
+      setLoading(true);
+     });
+  
+    router.events.on('routeChangeComplete', (url) => {
+      setLoading(false);
+    });
+  }, [router.events])
 
   return (
     <ChakraProvider theme={theme}>
       <Fonts />
       <Header />
-      {
-          
+      {    
         loading ? <Loading /> : <Component {...pageProps} /> 
       }
         <Footer />
