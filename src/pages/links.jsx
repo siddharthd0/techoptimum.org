b0e06c697b43fd207d6207b0ec06cbd7303e006b
Siddharth <96356209+siddharthd0@users.noreply.github.com>
2023-03-24 00:17:33 -0600

    improved links page with logos

--- src/pages/links.jsx
+++ src/pages/links.jsx
@@ -1,11 +1,26 @@
 import { Flex, Heading, Button, SimpleGrid } from "@chakra-ui/react";
 import Link from "next/link";
+import { FaDiscord, FaYoutube, FaTiktok, FaReddit, FaInstagram, FaTwitter, FaLinkedin } from 'react-icons/fa';
 
-function LinkButton({ href, text }) {
+function LinkButton({ href, text, icon }) {
+  const Icon = icon;
+
   return (
     <Link href={href}>
-      <Button bgColor="buttonBg.500" _hover={{ bgColor: "buttonBg.600" }} padding="40px" width="75%" marginBottom="3rem" transform="translate(15%, 0%)">
-        <Heading fontSize="2xl">{text}</Heading>
+      <Button
+        bgColor="buttonBg.500"
+        _hover={{ bgColor: "buttonBg.600" }}
+        _active={{
+          bgColor: "buttonBg.500",
+          transform: 'scale(.9)',
+        }}
+        padding="34px"
+        marginBottom="2rem"
+      >
+        <Flex alignItems="center">
+          <Icon size={24} style={{ marginRight: "11px" }} />
+          <Heading fontWeight="light" mt="2px" fontSize="2xl">{text}</Heading>
+        </Flex>
       </Button>
     </Link>
   );
@@ -15,16 +30,20 @@ export default function Links() {
   return (
     <>
       <Flex marginTop="3rem" direction="column" alignItems="center">
-        <Heading color="primary">Our Socials</Heading>
+        <Heading fontSize="5xl" color="primary">Our Links</Heading>
       </Flex>
-      <SimpleGrid marginTop="3rem" columns="3" alignItems="center" justifyContent="center" width="75%" marginLeft="12.5%">
-        <LinkButton href="https://discord.gg/J387natuAs" text="Discord" />
-        <LinkButton href="https://www.youtube.com/channel/UCLk-3L7DN7ShIrtKSO2ip4A" text="Youtube" />
-        <LinkButton href="https://www.tiktok.com/@techoptimum" text="TikTok" />
-        <LinkButton href="https://www.reddit.com/r/techoptimum" text="Reddit" />
-        <LinkButton href="https://www.instagram.com/techoptimum_" text="Instagram" />
-        <LinkButton href="https://twitter.com/TechOptimumHQ" text="Twitter" />
-        <LinkButton href="https://www.linkedin.com/company/techoptimum" text="LinkedIn" />
+      <SimpleGrid
+      margin="auto"
+      maxW="600px"
+        my="3rem !important"
+      >
+        <LinkButton href="https://discord.gg/J387natuAs" text="Discord" icon={FaDiscord} />
+        <LinkButton href="https://www.youtube.com/channel/UCLk-3L7DN7ShIrtKSO2ip4A" text="Youtube" icon={FaYoutube} />
+        <LinkButton href="https://www.tiktok.com/@techoptimum" text="TikTok" icon={FaTiktok} />
+        <LinkButton href="https://www.reddit.com/r/techoptimum" text="Reddit" icon={FaReddit} />
+        <LinkButton href="https://www.instagram.com/techoptimum_" text="Instagram" icon={FaInstagram} />
+        <LinkButton href="https://twitter.com/TechOptimumHQ" text="Twitter" icon={FaTwitter} />
+        <LinkButton href="https://www.linkedin.com/company/techoptimum" text="LinkedIn" icon={FaLinkedin} />
       </SimpleGrid>
     </>
   );
