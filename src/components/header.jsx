c4314491e122b011f5c750cd6fa93c73ef670dbd
Siddharth <96356209+siddharthd0@users.noreply.github.com>
2023-04-04 14:48:17 -0600

    Revert "Responsive navbar edit"

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -10,6 +10,7 @@ import {
   List,
   Text,
   Heading,
+  Hide,
   Menu,
   MenuButton,
   MenuItem,
@@ -21,12 +22,7 @@ import {
   UnorderedList,
   ListItem,
 } from "@chakra-ui/react";
-import {
-  ChevronRightIcon,
-  ChevronDownIcon,
-  HamburgerIcon,
-} from "@chakra-ui/icons";
-import { useBoolean } from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
 
 import Link from "next/link";
 import Head from "next/head";
@@ -44,21 +40,6 @@ export default function Header() {
     onClose: onSecondClose,
   } = useDisclosure();
   const buttonVariants = {
-    hidden: {
-      y: -10,
-      opacity: 0,
-    },
-    visible: {
-      y: 0,
-      opacity: 1,
-      transition: {
-        type: "fade",
-        damping: 10,
-        stiffness: 150,
-      },
-    },
-  };
-  const drawerButtonVariants = {
     hidden: {
       y: -10,
       opacity: 0,
@@ -374,10 +355,7 @@ export default function Header() {
             animate="visible"
             style={{ marginLeft: "auto", padding: "0.3rem" }}
           >
-            <ResponsiveHeader
-              buttonVariants={drawerButtonVariants}
-              buttonDelay
-            />
+            <ResponsiveHeader buttonVariants={buttonVariants} buttonDelay />
           </motion.div>
         </Flex>
       </Show>
@@ -387,20 +365,23 @@ export default function Header() {
 
 const ResponsiveHeader = ({ buttonVariants }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const [aboutIsOpen, setAboutIsOpen] = useBoolean();
-  const [initiativeIsOpen, setInitiativeIsOpen] = useBoolean();
+  const {
+    isOpen: isEditOpen,
+    onOpen: onEditOpen,
+    onClose: onEditClose,
+  } = useDisclosure();
+  const {
+    isOpen: isSecondOpen,
+    onOpen: onSecondOpen,
+    onClose: onSecondClose,
+  } = useDisclosure();
   const btnRef = React.useRef();
-  const buttonDelay = 0.2;
+  const buttonDelay = 0.3;
 
   return (
     <>
-      <Box padding="15px">
-        <HamburgerIcon 
-          ref={btnRef} 
-          onClick={onOpen} 
-          boxSize="9" 
-          color='#9da5f0'
-        />
+      <Box padding="19px">
+        <HamburgerIcon ref={btnRef} onClick={onOpen} boxSize="7" />
         <Box color="#060e17">
           <Drawer
             isOpen={isOpen}
@@ -410,7 +391,7 @@ const ResponsiveHeader = ({ buttonVariants }) => {
           >
             <DrawerOverlay />
             <DrawerContent>
-              <DrawerCloseButton size='lg' />
+              <DrawerCloseButton />
               <DrawerHeader>
                 <Flex alignItems={"center"} justifyContent={"start"}>
                   <Link _hover={{}} href={"./"}>
@@ -431,21 +412,21 @@ const ResponsiveHeader = ({ buttonVariants }) => {
               <DrawerBody>
                 <Flex direction="column">
                   <List className="nav-links">
-                    <motion.div
-                      variants={{
-                        ...buttonVariants,
-                        visible: {
-                          ...buttonVariants.visible,
-                          transition: {
-                            ...buttonVariants.visible.transition,
-                            delay: buttonDelay * 2,
+                    <Box>
+                      <motion.div
+                        variants={{
+                          ...buttonVariants,
+                          visible: {
+                            ...buttonVariants.visible,
+                            transition: {
+                              ...buttonVariants.visible.transition,
+                              // delay: buttonDelay * 2,
+                            },
                           },
-                        },
-                      }}
-                      initial="hidden"
-                      animate="visible"
-                    >
-                      <Box pb='5'>
+                        }}
+                        initial="hidden"
+                        animate="visible"
+                      >
                         <Text
                           className="link-navs link-navs-responsive"
                           color="primary"
@@ -454,26 +435,26 @@ const ResponsiveHeader = ({ buttonVariants }) => {
                             Home
                           </Link>
                         </Text>
-                      </Box>
-                    </motion.div>
-                    <motion.div
-                      variants={{
-                        ...buttonVariants,
-                        visible: {
-                          ...buttonVariants.visible,
-                          transition: {
-                            ...buttonVariants.visible.transition,
-                            delay: buttonDelay * 3.5,
-                          },
-                        },
-                      }}
-                      initial="hidden"
-                      animate="visible"
-                      className="link-navs-responsive"
-                      onClick={() => setAboutIsOpen.toggle()}
-                    >
-                      <Box pb='5'>
-                        <Flex>
+                      </motion.div>
+                    </Box>
+                    <Box>
+                      <Menu gutter={"5"} isOpen={isEditOpen}>
+                        <motion.div
+                          variants={{
+                            ...buttonVariants,
+                            visible: {
+                              ...buttonVariants.visible,
+                              transition: {
+                                ...buttonVariants.visible.transition,
+                                // delay: buttonDelay * 3.5,
+                              },
+                            },
+                          }}
+                          initial="hidden"
+                          animate="visible"
+                          className="link-navs-responsive"
+                        >
+                          {/* need to add open & close functionality */}
                           <Text
                             className="link-navs"
                             bgColor={"transparent"}
@@ -481,60 +462,97 @@ const ResponsiveHeader = ({ buttonVariants }) => {
                           >
                             About
                           </Text>
-                          {aboutIsOpen ? (
-                            <ChevronDownIcon
-                              boxSize="5"
-                              className="link-navs-dropdown-responsive"
-                            />
-                          ) : (
-                            <ChevronRightIcon
-                              boxSize="5"
-                              my="auto"
-                              className="link-navs-dropdown-responsive"
-                            />
-                          )}
-                        </Flex>
-                        {aboutIsOpen && (
-                          <Box className="link-navs-responsive" >
+                        </motion.div>
+                        {/* add condition to show when about item active */}
+                        <Show>
+                          <Box className="link-navs-responsive">
                             <UnorderedList display="flex" flexDir="column">
                               <ListItem display="block" className="link-navs">
                                 <Link href="/about">
-                                  <Text py='2'>Team</Text>
+                                  <MenuItem>Team</MenuItem>
                                 </Link>
                               </ListItem>
                               <ListItem display="block" className="link-navs">
                                 <Link href="/contact">
-                                  <Text py='2'>Contact</Text>
+                                  <MenuItem>Contact</MenuItem>
                                 </Link>
                               </ListItem>
                               <ListItem display="block" className="link-navs">
                                 <Link href="/faq">
-                                  <Text pt='2'>FAQ</Text>
+                                  <MenuItem>FAQ</MenuItem>
                                 </Link>
                               </ListItem>
                             </UnorderedList>
                           </Box>
-                        )}
-                      </Box>
-                    </motion.div>
-                    <Box pb='5'>
-                      <motion.div
-                        variants={{
-                          ...buttonVariants,
-                          visible: {
-                            ...buttonVariants.visible,
-                            transition: {
-                              ...buttonVariants.visible.transition,
-                              delay: buttonDelay * 5,
+                        </Show>
+                      </Menu>
+                      <Menu gutter={"5"} isOpen={isEditOpen}>
+                        <motion.div
+                          variants={{
+                            ...buttonVariants,
+                            visible: {
+                              ...buttonVariants.visible,
+                              transition: {
+                                ...buttonVariants.visible.transition,
+                                delay: buttonDelay * 3.5,
+                              },
                             },
-                          },
-                        }}
-                        initial="hidden"
-                        animate="visible"
-                        className="link-navs-responsive"
-                        onClick={() => setInitiativeIsOpen.toggle()}
-                      >
-                        <Flex>
+                          }}
+                          initial="hidden"
+                          animate="visible"
+                        >
+                          <MenuButton
+                            className="link-navs"
+                            bgColor={"transparent"}
+                            color="primary"
+                            onMouseEnter={onEditOpen}
+                            onMouseLeave={onEditClose}
+                          >
+                            About
+                          </MenuButton>
+                        </motion.div>
+                        <div className="testing-nav">
+                          <MenuList
+                            onMouseEnter={onEditOpen}
+                            onMouseLeave={onEditClose}
+                            className="link-navs-dropdown"
+                          >
+                            <Link href="/about">
+                              <MenuItem className="link-navs-dd-text">
+                                Team
+                              </MenuItem>
+                            </Link>
+                            <Link href="/contact">
+                              <MenuItem className="link-navs-dd-text">
+                                Contact
+                              </MenuItem>
+                            </Link>
+                            <Link href="/faq">
+                              <MenuItem className="link-navs-dd-text">
+                                FAQ
+                              </MenuItem>
+                            </Link>
+                          </MenuList>
+                        </div>
+                      </Menu>
+                    </Box>
+                    <Box>
+                      <Menu gutter={"5"} isOpen={isSecondOpen}>
+                        <motion.div
+                          variants={{
+                            ...buttonVariants,
+                            visible: {
+                              ...buttonVariants.visible,
+                              transition: {
+                                ...buttonVariants.visible.transition,
+                                // delay: buttonDelay * 5,
+                              },
+                            },
+                          }}
+                          initial="hidden"
+                          animate="visible"
+                          className="link-navs-responsive"
+                        >
                           <Text
                             className="link-navs"
                             bgColor={"transparent"}
@@ -542,58 +560,47 @@ const ResponsiveHeader = ({ buttonVariants }) => {
                           >
                             Initiatives
                           </Text>
-                          {initiativeIsOpen ? (
-                            <ChevronDownIcon
-                              boxSize="5"
-                              className="link-navs-dropdown-responsive"
-                            />
-                          ) : (
-                            <ChevronRightIcon
-                              boxSize="5"
-                              my="auto"
-                              className="link-navs-dropdown-responsive"
-                            />
-                          )}
-                        </Flex>
-                        {initiativeIsOpen && (
+                        </motion.div>
+                        {/* write conditional to show when initiative tab is active */}
+                        <Show>
                           <Box className="link-navs-responsive">
                             <UnorderedList display="flex" flexDir="column">
                               <ListItem className="link-navs">
                                 <Link href="./curriculum">
-                                  <Text py='2'>Courses</Text>
+                                  <MenuItem>Courses</MenuItem>
                                 </Link>
                               </ListItem>
                               <ListItem className="link-navs">
                                 <Link href="/hackathon">
-                                  <Text py='2'>Hackathon</Text>
+                                  <MenuItem>Hackathon</MenuItem>
                                 </Link>
                               </ListItem>
                               <ListItem className="link-navs">
                                 <Link href="/podcast">
-                                  <Text py='2'>Talks</Text>
+                                  <MenuItem>Talks</MenuItem>
                                 </Link>
                               </ListItem>
                             </UnorderedList>
                           </Box>
-                        )}
-                      </motion.div>
+                        </Show>
+                      </Menu>
                     </Box>
-                    <motion.div
-                      variants={{
-                        ...buttonVariants,
-                        visible: {
-                          ...buttonVariants.visible,
-                          transition: {
-                            ...buttonVariants.visible.transition,
-                            delay: buttonDelay * 6.5,
+                    <Box>
+                      <motion.div
+                        variants={{
+                          ...buttonVariants,
+                          visible: {
+                            ...buttonVariants.visible,
+                            transition: {
+                              ...buttonVariants.visible.transition,
+                              // delay: buttonDelay * 3.5,
+                            },
                           },
-                        },
-                      }}
-                      initial="hidden"
-                      animate="visible"
-                      className="link-navs-responsive"
-                    >
-                      <Box  pb='5'>
+                        }}
+                        initial="hidden"
+                        animate="visible"
+                        className="link-navs-responsive"
+                      >
                         <Text
                           className="link-navs"
                           bgColor={"transparent"}
@@ -601,23 +608,23 @@ const ResponsiveHeader = ({ buttonVariants }) => {
                         >
                           <Link href="/join-team">Volunteer</Link>
                         </Text>
-                      </Box>
-                    </motion.div>
-                    <motion.div
-                      variants={{
-                        ...buttonVariants,
-                        visible: {
-                          ...buttonVariants.visible,
-                          transition: {
-                            ...buttonVariants.visible.transition,
-                            delay: buttonDelay * 8,
+                      </motion.div>
+                    </Box>
+                    <Box className="link-navs link-navs-responsive">
+                      <motion.div
+                        variants={{
+                          ...buttonVariants,
+                          visible: {
+                            ...buttonVariants.visible,
+                            transition: {
+                              ...buttonVariants.visible.transition,
+                              // delay: buttonDelay * 8,
+                            },
                           },
-                        },
-                      }}
-                      initial="hidden"
-                      animate="visible"
-                    >
-                      <Box className="link-navs link-navs-responsive" pb='5'>
+                        }}
+                        initial="hidden"
+                        animate="visible"
+                      >
                         <Link
                           _hover={{
                             cursor: "pointer",
@@ -631,15 +638,15 @@ const ResponsiveHeader = ({ buttonVariants }) => {
                             color="primary"
                             _hover={{
                               cursor: "pointer",
-                              color: '#ffffff',
+
                               opacity: "0.7 !important",
                             }}
                           >
                             Donate
                           </Text>
                         </Link>
-                      </Box>
-                    </motion.div>
+                      </motion.div>
+                    </Box>
                   </List>
                 </Flex>
               </DrawerBody>
