8d1506344ce896f1ec1e804677f96f86beb8069b
Siddharth <96356209+siddharthd0@users.noreply.github.com>
2023-06-19 01:50:18 -0600

    sitemap

---
+++ src/pages/api/sitemap.js
@@ -0,0 +1,30 @@
+const { SitemapStream, streamToPromise } = require('sitemap');
+const { Readable } = require('stream');
+
+export default async (req, res) => {
+  try {
+    const links = [
+      { url: '/', changefreq: 'daily', priority: 1.0 },
+      { url: '/volunteer', changefreq: 'weekly', priority: 0.8 },
+      { url: '/hackathon', changefreq: 'weekly', priority: 0.8 },
+      { url: '/courses', changefreq: 'weekly', priority: 0.8 },
+      { url: '/donate', changefreq: 'monthly', priority: 0.5 },
+      // Add more URLs as needed
+    ];
+
+    const stream = new SitemapStream({ hostname: 'https://www.techoptimum.org' });
+
+    res.writeHead(200, {
+      'Content-Type': 'application/xml',
+    });
+
+    const xmlString = await streamToPromise(
+      Readable.from(links).pipe(stream)
+    ).then((data) => data.toString());
+
+    res.end(xmlString);
+  } catch (error) {
+    console.error(error);
+    res.status(500).end();
+  }
+};
