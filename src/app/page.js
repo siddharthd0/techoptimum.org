7fb7ca168c9d9aa72e7961c09bd01ae74328d553
Rayane Benamre <benamrerayane@gmail.com>
2023-07-24 19:51:13 +0100

    Added fade in animation to home page

--- src/app/page.js
+++ src/app/page.js
@@ -4,16 +4,18 @@ import HeroHeader from "@/components/homepage-landing";
 import Courses from "@/components/courses";
 import Finder from "@/components/internship-finder";
 import Approach from "@/components/approach";
-import { Box } from "@chakra-ui/react";
+import { Box, Fade } from "@chakra-ui/react";
 
 export default function Home() {
 
   return (
-    <Box my={["20px", "130px"]}>
-      <HeroHeader />
-      <Courses />
-      <Approach />
-      <Finder />
-    </Box>
+    <Fade in={true}>
+      <Box my={["20px", "130px"]}>
+        <HeroHeader />
+        <Courses />
+        <Approach />
+        <Finder />
+      </Box>
+    </Fade>
   );
 }
