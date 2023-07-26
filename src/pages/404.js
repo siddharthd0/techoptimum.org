0c9a292ea33cc828984e9d87628420455a50c6cf
Aditya Sahasranam <adityasahas2025@gmail.com>
2023-07-25 21:13:27 -0700

    LMAO SUCKS TO BE YOU

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,4 @@
-import ErrorPage from "../components/ErrorPage";
+import ErrorPage from "../components/errorpage";
 import RootLayout from "../app/layout";
 export default function ErrorPages() {
   return (
