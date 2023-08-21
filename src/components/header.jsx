559ce0a601dc9f13ca357f7171a0f95ccf9ff841
Siddharth <96356209+siddharthd0@users.noreply.github.com>
2023-08-21 15:36:38 -0600

    add

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -63,8 +63,6 @@ export default function WithSubnavigation() {
   const { isOpen, onToggle } = useDisclosure();
   const [NAV_ITEMS, setNAV_ITEMS] = useState(DefaultNavItems);
   const [data, setData] = useState({});
-  
-
 
 
   useEffect(() => {
