bf684511f9aef3e727db14c958ff83c12aaebebc
Siddharth <96356209+siddharthd0@users.noreply.github.com>
2023-08-27 18:28:46 -0600

    added minneapolis redirect

--- next.config.js
+++ next.config.js
@@ -26,6 +26,12 @@ const nextConfig = {
                 destination: "https://techoptimum.notion.site/Volunteer-Benefits-at-Tech-Optimum-18b3263dda344832a8df1687df92a8f4",
                 permanent: false,
                 basePath: false,
+            },
+            {
+                source: "/minneapolis",
+                destination: "https://forms.gle/TdZ3Fz8hkXZsvfu1A",
+                permanent: false,
+                basePath: false,
             }
         ];
     },
