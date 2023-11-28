8383e99d023ceabee8958ffd4eb8718564472c30
Siddharth <96356209+siddharthd0@users.noreply.github.com>
2023-11-27 21:36:55 -0600

    features of dashboard

--- src/app/page.js
+++ src/app/page.js
@@ -82,6 +82,9 @@ export default function Home() {
 
       <Sponsors sponsors={sponsorsData} />
       <Courses />
+     { /*<ProgressCheck />
+      <ChapterContent />
+      <TechGuru/>*/}
     
       <Features />
       <Approach />
